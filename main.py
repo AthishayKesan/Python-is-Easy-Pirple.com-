658821ee1bc5202641036d2c9e2815a9867155a2
AthishayKesan <74936818+AthishayKesan@users.noreply.github.com>
2020-11-24 04:19:02 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,79 @@
+
+"""
+Pirple.com : Python is Easy
+Chapter 1 : Variables
+Homework#1
+"""
+
+Song = "Dance Monkey" #String
+Genere = "Electropop" #String
+LyricistFirstName = "Toni"#String
+LyricistLastName = "Watson" #String
+LyricistFullName =  LyricistFirstName + " " + LyricistLastName #String combine
+Producer = "Konstantin Kerstring" #String
+DurationInSeconds = 209 #Integer
+ReleaseDate = "10 May, 2019" #String
+NumberOFViewsOnYoutubeinMillion = 87.16#Float
+ApproxNumberOfWords = 434#Interger
+
+
+print("Song Name : ",Song)
+print("Song Genere : ",Genere)
+print("Lyricist Name : ",LyricistFullName)
+print("Song Producer : ",Producer)
+print("Song Durtion(in seconds) : ",DurationInSeconds)
+print("Song Release Date : ",ReleaseDate)
+print("Number of views on Youtube : ",NumberOFViewsOnYoutubeinMillion)
+print("Number of words in the song : ",ApproxNumberOfWords)
+
+"""
+Lyrics
+ They say oh my god I see the way you shine
+ Take your hand, my dear, and place them both in mine
+ You know you stopped me dead while I was passing by
+ And now I beg to see you dance just one more time
+ Ooh I see you, see you, see you every time
+ And oh my I, I, I like your style
+ You, you make me, make me, make me wanna cry
+ And now I beg to see you dance just one more time
+ So they say
+ Dance for me, dance for me, dance for me, oh, oh, oh
+ I've never seen anybody do the things you do before
+ They say move for me, move for me, move for me, ay, ay, ay
+ And when you're done I'll make you do it all again
+ I said oh my god I see you walking by
+ Take my hands, my dear, and look me in my eyes
+ Just like a monkey I've been dancing my whole life
+ But you just beg to see me dance just one more time
+ Ooh I see you, see you, see you every time
+ And oh my I, I like your style
+ You, you make me, make me, make me wanna cry
+ And now I beg to see you dance just one more time
+ So they say
+ Dance for me, dance for me, dance for me, oh, oh, oh
+ I've never seen anybody do the things you do before
+ They say move for me, move for me, move for me, ay, ay, ay
+ And when you're done I'll make you do it all again
+ They say
+ Dance for me, dance for me, dance for me, oh, oh, oh, oh, oh, oh, oh
+ I've never seen anybody do the things you do before
+ They say move for me, move for me, move for me, ay, ay, ay
+ And when you're done I'll make you do it all again
+ Ooh
+ Woah-oh, woah-oh, oh
+ Ooh
+ Ah ah, ah
+ They say
+ Dance for me, dance for me, dance for me, oh, oh, oh
+ I've never seen anybody do the things you do before
+ They say move for me, move for me, move for me, ay, ay, ay
+ And when you're done I'll make you do it all again
+ They say
+ Dance for me, dance for me, dance for me, oh, oh, oh, oh, oh, oh, oh
+ I've never seen anybody do the things you do before
+ They say move for me, move for me, move for me, ay, ay, ay
+ And when you're done I'll make you do it all again
+ All again
+"""
+
+
